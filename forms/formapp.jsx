22d30c8bb21b31595b20a28e79972f0d3eb3913b
Pranay Salve <64769068+pranaysalve@users.noreply.github.com>
2023-07-26 21:42:57 +0530

    sdasd

--- forms/formapp.jsx
+++ forms/formapp.jsx
@@ -81,11 +81,11 @@ export default function App() {
               </div>
             ) : (
               <>
-                <Link href="/viewForm">
+                <a href="/viewForm">
                   <a className="pt-10 text-white bg-blue-500 hover:bg-blue-700 focus:outline-none focus:ring-4 focus:ring-blue-300 font-medium rounded-full text-sm px-5 py-2.5 text-center mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
                     View Forms
                   </a>
-                </Link>
+                </a>
               </>
             )}
           </form>
